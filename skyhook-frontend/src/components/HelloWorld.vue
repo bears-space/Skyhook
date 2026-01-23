a788ca82a8586b9d47a2f6c388864dadc82fb15e
iHouqLF <73959649+JoelHer@users.noreply.github.com>
2026-01-23 17:33:34 +0100

    Basic WebSock implementation

--- skyhook-frontend/src/components/HelloWorld.vue
+++ skyhook-frontend/src/components/HelloWorld.vue
@@ -1,11 +1,31 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, onMounted, onBeforeUnmount } from "vue";
+import { socket } from "../socket";
 
 defineProps({
   msg: String,
 })
 
 const count = ref(0)
+
+
+const messages = ref([]);
+
+function onMessage(payload) {
+  messages.value.push(payload);
+}
+
+onMounted(() => {
+  socket.on("message", onMessage);
+
+  // example emit
+  socket.emit("hello", { from: "vue" });
+});
+
+onBeforeUnmount(() => {
+  socket.off("message", onMessage); // important: remove handler
+});
+
 </script>
 
 <template>
@@ -19,21 +39,10 @@ const count = ref(0)
     </p>
   </div>
 
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Learn more about IDE Support for Vue in the
-    <a
-      href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
-      target="_blank"
-      >Vue Docs Scaling up Guide</a
-    >.
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <div>
+    <h3>WS_Messages</h3>
+    <pre>{{ messages }}</pre>
+  </div>
 </template>
 
 <style scoped>
--- skyhook-frontend/src/main.js
+++ skyhook-frontend/src/main.js
@@ -1,5 +1,15 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { socket } from "./socket";
 
 createApp(App).mount('#app')
+
+
+// Socket stuff
+socket.connect();
+
+socket.on("connect", () => console.log("✅ socket connected", socket.id));
+socket.on("disconnect", (reason) => console.log("❌ socket disconnected", reason));
+
+app.mount("#app");
