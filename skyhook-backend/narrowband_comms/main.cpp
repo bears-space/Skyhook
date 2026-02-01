bd51039ca6ef2ecc9878ac9d8b5b03e489f804f3
Till Krahmer <kradu526@gmail.com>
2026-02-01 02:37:38 +0100

    added some rough sketches on how socket event handling will look like in cpp

--- skyhook-backend/narrowband_comms/main.cpp
+++ skyhook-backend/narrowband_comms/main.cpp
@@ -3,6 +3,29 @@
 
 #include <sio_client.h>
 
+#include <queue>
+#include <mutex>
+#include <optional>
+#include <string>
+
+std::queue<std::string> event_queue;
+std::mutex queue_mutex;
+
+void enqueue_event(const std::string &cmd) {
+    std::lock_guard<std::mutex> lock(queue_mutex);
+    event_queue.push(cmd);
+}
+
+std::optional<std::string> get_next_event() {
+    std::lock_guard<std::mutex> lock(queue_mutex);
+    if (!event_queue.empty()) {
+        std::string cmd = event_queue.front();
+        event_queue.pop();
+        return cmd;
+    }
+    return std::nullopt;
+}
+
 // create a new instance of the HAL class
 // use SPI channel 0
 // the LLC68 CS is connected to CE0
@@ -17,6 +40,18 @@ LLCC68 radio = new Module(hal, 8, 25, 23, 24);
 
 // the entry point for the program
 int main(int argc, char** argv) {
+
+  // init socketio socketio client
+  // TODO: pack the init code into seperate functions
+  sio::client c;
+
+  c.socket()->on("command", [&](sio::event &ev) {
+        std::string cmd = ev.get_message()->get_map()["cmd"]->get_string();
+        enqueue_event(cmd); // thread-safe push
+    });
+
+  c.connect("http://localhost:5000");
+
   // initialize just like with Arduino
   printf("[LLCC68] Initializing ... ");
   int state = radio.beginFSK();
@@ -32,23 +67,30 @@ int main(int argc, char** argv) {
 
   // loop forever
   int count = 0;
-  for(;;) {
-    // send a packet
-    printf("[LLCC68] Transmitting packet ... ");
-    char str[64];
-    sprintf(str, "Hello World! #%d", count++);
-    state = radio.transmit(str);
-    if(state == RADIOLIB_ERR_NONE) {
-      // the packet was successfully transmitted
-      printf("success!\n");
+  while(1) {
 
-      // wait for a second before transmitting again
-      hal->delay(1000);
+    auto maybe_cmd = get_next_event();
+    if (maybe_cmd) {
+        std::string cmd = *maybe_cmd;
+        std::cout << "Processing command in main loop: " << cmd << std::endl;
 
-    } else {
-      printf("failed, code %d\n", state);
+        // Do some sending here
+        printf("[LLCC68] Transmitting packet ... ");
+        char str[64];
+        sprintf(str, "Hello World! #%d", count++); // obviously this should send the command instead of hello world
+        state = radio.transmit(str);
+        if(state == RADIOLIB_ERR_NONE) {
+          // the packet was successfully transmitted
+          printf("success!\n");
 
+        } else {
+          printf("failed, code %d\n", state);
+
+        }
     }
+
+    // TODO: add receiving
+    hal->delay(1000);
 
   }
 
